e34494206542666a2f2d0e8c2366cb838252b60b
MichaelMounes <michel1998114@gmail.com>
2023-08-11 17:40:10 -0700

    new

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,18 +1,16 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 int main(void)
 {
 	int n;
-
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
+	n=rand() - RAND_MAX/2;
+	if(n>0)
 	{
 		printf("%d is positive\n", n);
 	}
-	else if (n == 0)
+	else if (n==0)
 	{
 		printf("%d is zero\n", n);
 	}
@@ -21,4 +19,5 @@ int main(void)
 		printf("%d is negative\n", n);
 	}
 	return (0);
+
 }
